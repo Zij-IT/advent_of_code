ab2abcfd2933319a3c460aa547fca03c643cd25b
Zij-IT <elijah.reed@hartvigsen.xyz>
2021-12-06 21:44:17 +0100

    Day 1: Found pattern while simplifying zips

--- src/D1.hs
+++ src/D1.hs
@@ -7,14 +7,14 @@ module D1
 format :: String -> [Int]
 format = map read . lines
 
-windows :: [a] -> [[a]]
-windows xs = zipWith (\x y -> [x, y]) xs (drop 1 xs)
+pairs :: Int -> [Int] -> [(Int, Int)]
+pairs n xs = zip xs (drop n xs)
 
-tripletSum :: [Int] -> [Int]
-tripletSum xs = zipWith3 (\x y z -> x + y + z) xs (drop 1 xs) (drop 2 xs)
+solveN :: Int -> [Int] -> Int
+solveN n = length . filter (uncurry (<)) . pairs n
 
 part1 :: [Int] -> Int
-part1 = length . filter (\[x, y] -> y > x) . windows
+part1 = solveN 1
 
 part2 :: [Int] -> Int
-part2 = part1 . tripletSum
+part2 = solveN 3
