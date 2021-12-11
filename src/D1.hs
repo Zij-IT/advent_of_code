9f44247708e49656655a5731679ec9753c41e935
Zij-IT <elijah.reed@hartvigsen.xyz>
2021-12-11 21:08:25 +0100

    D1: Added explanation!

--- src/D1.hs
+++ src/D1.hs
@@ -15,6 +15,16 @@ pairs n xs = zip xs (drop n xs)
 solveN :: Int -> Input -> Int
 solveN n = length . filter (uncurry (<)) . pairs n
 
+-- Explanation:
+-- Part1: Solved simply by comparing one element with the next one in the list.
+--
+-- Part2: The question asks to find all instances in a list where
+--        SUM [a, b, c] < SUM [b, c, d] for any consecutive a, b, c, d
+--        in the list. This can be solved very similarly to Part 1.
+--        For any [a, b, c, d | a, b, c, d ELEM Z] it is true that:
+--        a + b + c < b + c + d <=> a < d
+--        Therefore we can solve this by comparing a and d, skipping the addition!
+
 part1 :: Input -> Int
 part1 = solveN 1
 
