d43af852e6f94ab0e97c4b395561863646a8ed20
Zij-IT <elijah.reed@hartvigsen.xyz>
2021-12-17 14:32:56 +0100

    D16: Deduplication

--- src/D16.hs
+++ src/D16.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
 module D16
   ( format
   , part1
@@ -7,7 +8,6 @@ module D16
 
 import Data.Maybe (fromJust)
 import Data.Bifunctor (first)
-import Control.Arrow ((***))
 
 type Input = [Int]
 type Output = Int
@@ -37,10 +37,7 @@ binListToDecimal = foldl (\acc x -> 2 * acc + x) 0
 getLiteral :: [Int] -> Maybe ([Int], [Int])
 getLiteral lit = case lit of
   (0:xs) -> Just $ splitAt 4 xs
-  (1:xs) ->
-    let (bits, rest) = splitAt 4 xs
-    in first (bits ++) <$> getLiteral rest
-  _ -> error "Bad literal parse"
+  (1:xs) -> first (take 4 xs ++) <$> getLiteral (drop 4 xs)
 
 parseLiteral :: [Int] -> [Int] -> Maybe (Packet, [Int])
 parseLiteral v xs = first (Literal (binListToDecimal v) . binListToDecimal) <$> getLiteral xs
@@ -50,28 +47,21 @@ parsePackets xs = case parseHierarchy xs of
   Just (packet, rest) -> packet : parsePackets rest
   Nothing -> []
 
-parseNPackages :: [Int] -> Int -> ([Packet], [Int])
-parseNPackages xs n = first reverse . fromJust . (!! n) $ iterate conv $ Just ([], xs)
+parseNPackages :: Int -> [Int] -> ([Packet], [Int])
+parseNPackages n xs = first reverse . fromJust . (!! n) $ iterate conv $ Just ([], xs)
   where conv = \(Just (packets, xs)) -> first (:packets) <$> parseHierarchy xs
 
 parseOperator :: [Int] -> [Int] -> [Int] -> Maybe (Packet, [Int])
-parseOperator v t (lengthId : xs)
-  | null xs = Nothing
-  | lengthId == 0 =
-    let (lenBits, rest) = splitAt 15 xs
-        len = binListToDecimal lenBits
-        ver = binListToDecimal v
+parseOperator v t (lengthId : xs) = if null xs then Nothing else
+    let leng = if lengthId == 0 then 15 else 11
+        (packLeng, rest) = first binListToDecimal $ splitAt leng xs
+        f = if lengthId == 0
+          then first parsePackets . splitAt packLeng 
+          else parseNPackages packLeng
+        version = binListToDecimal v
         func = funcFromType t
-        (packets, rest') = splitAt len rest
-        packages = parsePackets packets
-    in Just (Operator ver packages func, rest')
-  | otherwise =
-    let (countBits, rest) = splitAt 11 xs
-        count = binListToDecimal countBits
-        ver = binListToDecimal v
-        func = funcFromType t
-        (packages, rest') = parseNPackages rest count
-    in Just (Operator ver packages func, rest')
+        (packages, rest') = f rest
+    in Just (Operator version packages func, rest')
 
 parsePacket :: [Int] -> [Int] -> [Int] -> Maybe (Packet, [Int])
 parsePacket _ _ [] = Nothing
